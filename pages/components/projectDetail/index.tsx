fe77a80fe9c413c1d0cf054ba29bb29f5d920e77
dangtuanbinh <tuanbinh.35@gmail.com>
2023-05-31 17:00:36 +0700

    fix name

--- pages/components/projectDetail/index.tsx
+++ pages/components/projectDetail/index.tsx
@@ -34,61 +34,65 @@ const ProjectDetail: React.FC<IProjectDetailProps> = (props) => {
         padding: animation ? 0 : "20px 60px",
       }}
     >
-      <section className={`${classNamePrefix}__content`}>
-        <section className={`${classNamePrefix}__content-main`}>
-          <div className={`${classNamePrefix}__floated-item`}>
+      {projectSelected ? (
+        <section className={`${classNamePrefix}__content`}>
+          <section className={`${classNamePrefix}__content-main`}>
+            <div className={`${classNamePrefix}__floated-item`}>
+              <div
+                className={`${classNamePrefix}__content-main-image-wrapper`}
+                style={{
+                  width: animation ? "100vw" : 450,
+                  height: animation ? "100vh" : 550,
+                  marginLeft: animation ? 0 : 161,
+                }}
+              >
+                <div
+                  className={`${classNamePrefix}__content-main-image`}
+                  style={{
+                    backgroundImage: `url(${project1.src})`,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                ></div>
+
+                <div
+                  className={`${classNamePrefix}__content-main-image-layer`}
+                ></div>
+              </div>
+            </div>
+
             <div
-              className={`${classNamePrefix}__content-main-image-wrapper`}
+              className={`${classNamePrefix}__content-main-text-wrapper`}
               style={{
-                width: animation ? "100vw" : 450,
-                height: animation ? "100vh" : 550,
-                marginLeft: animation ? 0 : 161,
+                bottom: animation ? -400 : 200,
+                display: animation ? "none" : "block",
               }}
             >
-              <div
-                className={`${classNamePrefix}__content-main-image`}
-                style={{
-                  backgroundImage: `url(${project1.src})`,
-                  width: "100%",
-                  height: "100%",
-                }}
-              ></div>
+              <div className={`${classNamePrefix}__content-main-title`}>
+                <span>{projectSelected.name ? projectSelected.name : "No name"}</span>
+              </div>
 
-              <div
-                className={`${classNamePrefix}__content-main-image-layer`}
-              ></div>
+              <div className={`${classNamePrefix}__content-main-text`}>
+                {projectSelected.content ? projectSelected.content : "No content"}
+              </div>
             </div>
-          </div>
+          </section>
 
-          <div
-            className={`${classNamePrefix}__content-main-text-wrapper`}
-            style={{
-              bottom: animation ? -400 : 200,
-              display: animation ? "none" : "block",
-            }}
-          >
-            <div className={`${classNamePrefix}__content-main-title`}>
-              <span>{projectSelected.name}</span>
-            </div>
+          <section className={`${classNamePrefix}__content-sub`}>
+            <div className={`${classNamePrefix}__content-sub-text-wrapper`}>
+              <div className={`${classNamePrefix}__content-main-title`}>
+                <span>{projectSelected.name ? projectSelected.name : "No name"}</span>
+              </div>
 
-            <div className={`${classNamePrefix}__content-main-text`}>
-              {projectSelected.content}
+              <div className={`${classNamePrefix}__content-main-text`}>
+                {projectSelected.subContent ? projectSelected.subContent : "No content"}
+              </div>
             </div>
-          </div>
+          </section>
         </section>
-
-        <section className={`${classNamePrefix}__content-sub`}>
-          <div className={`${classNamePrefix}__content-sub-text-wrapper`}>
-            <div className={`${classNamePrefix}__content-main-title`}>
-              <span>{projectSelected.name}</span>
-            </div>
-
-            <div className={`${classNamePrefix}__content-main-text`}>
-              {projectSelected.subContent}
-            </div>
-          </div>
-        </section>
-      </section>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
