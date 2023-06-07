3104989a885ffe93f6990d42c5cc7dded184d7fb
dangtuanbinh <tuanbinh.35@gmail.com>
2023-06-08 01:43:45 +0700

    fix page icon

--- pages/_app.js
+++ pages/_app.js
@@ -94,8 +94,25 @@ function MyApp({ Component, ...rest }) {
     <>
       <DefaultSeo {...SEO} />
       <Head>
-        <link rel="shortcut icon" href="/public/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />      </Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <Provider store={store}>
         {isPrivateMenu && <TopMenu />}
         <div className="layout">
