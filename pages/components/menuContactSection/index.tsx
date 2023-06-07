25b36ee1c99fa5b71c87abfe84f95379e9bbcc7a
dangtuanbinh <tuanbinh.35@gmail.com>
2023-06-07 15:49:57 +0700

    fix phone call

--- pages/components/menuContactSection/index.tsx
+++ pages/components/menuContactSection/index.tsx
@@ -6,13 +6,12 @@ const MenuContactSection = () => {
   return (
     <div className={classNamePrefix}>
       <div className={`${classNamePrefix}__info`}>
-        <a href="(+84)913999345">(+84) 913 999 345</a>
-        <a href = "mailto: info@foolist.vn">info@foolist.vn</a>
+        <a href="tel:(+84)913999345">(+84) 913 999 345</a>
+        <a href="mailto: info@foolist.vn">info@foolist.vn</a>
       </div>
 
       <div className={`${classNamePrefix}__address`}>
-        Floor 3,64 Vo Thi Sau, Tan Dinh Ward, District 1,
-       HCM, Viet Nam
+        Floor 3,64 Vo Thi Sau, Tan Dinh Ward, District 1, HCM, Viet Nam
       </div>
     </div>
   );
