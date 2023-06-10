b5ffe2e6d63e1f9e3fd9942cb1c0ca3e0ce4100d
dangtuanbinh <tuanbinh.35@gmail.com>
2023-06-10 11:43:13 +0700

    fix language service section

--- pages/components/serviceSection/index.tsx
+++ pages/components/serviceSection/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import CustomNavigationButton from "../customNavigationButton";
 import { useRouter } from "next/router";
 import { AnimationOnScroll } from "react-animation-on-scroll";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   // Add custom props here
@@ -15,13 +16,14 @@ const ServiceSection = (
   _props: InferGetStaticPropsType<typeof getStaticProps>
 ) => {
   const router = useRouter();
+  const {t} = useTranslation()
 
   return (
     <div className={classNamePrefix}>
       <div className={`${classNamePrefix}__title-wrapper`}>
         <AnimationOnScroll animateIn="animate__slideInUp" animateOnce={true}>
           <div className={`${classNamePrefix}__title`}>
-            Dịch vụ của chúng tôi
+            {t("service.title")}
           </div>
         </AnimationOnScroll>
 
@@ -36,15 +38,15 @@ const ServiceSection = (
         <div className={`${classNamePrefix}__content-item`}>
           <AnimationOnScroll animateIn="animate__slideInUp" animateOnce={true}>
             <div className={`${classNamePrefix}__content-item-title`}>
-              Media Development
+              {t("service.media")}
             </div>
           </AnimationOnScroll>
 
           <AnimationOnScroll animateIn="animate__slideInUp" animateOnce={true}>
             <div className={`${classNamePrefix}__content-item-list`}>
               <ul>
-                <li>TVC</li>
-                <li>Product Design</li>
+                <li>{t("service.tvc")}</li>
+                <li>{t("service.product-design")}</li>
               </ul>
             </div>
           </AnimationOnScroll>
@@ -55,16 +57,16 @@ const ServiceSection = (
         <div className={`${classNamePrefix}__content-item`}>
           <AnimationOnScroll animateIn="animate__slideInUp" animateOnce={true}>
             <div className={`${classNamePrefix}__content-item-title`}>
-              Information Technology
+              {t("service.technology-development")}
             </div>
           </AnimationOnScroll>
 
           <AnimationOnScroll animateIn="animate__slideInUp" animateOnce={true}>
             <div className={`${classNamePrefix}__content-item-list`}>
               <ul>
-                <li>Website development</li>
-                <li>Mobile application</li>
-                <li>ERP</li>
+                <li>{t("service.website-development")}</li>
+                <li>{t("service.mobile-app-development")}</li>
+                <li>{t("service.erp")}</li>
               </ul>
             </div>
           </AnimationOnScroll>
