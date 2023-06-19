2e2bc348d091cd7a723732e1aba8bf21e06b948a
dangtuanbinh <tuanbinh.35@gmail.com>
2023-06-19 12:09:01 +0700

    fix export default function

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -2,27 +2,32 @@ import sendgrid from "@sendgrid/mail";
 
 sendgrid.setApiKey(process.env.NEXT_PUBLIC_MAIL_API_KEY);
 
-export async function sendEmail(req, res) {
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+
   if (req.method === "POST") {
     try {
       await sendgrid.send({
-        to: "info@foolist.vn",
+        to: "tuanbinh.35@gmail,com",
         from: "binhtuandang35@gmail.com",
         subject: "Request for more information about services.",
         html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html lang="en">
         <head>
           <meta charset="utf-8">
-  
+
           <title>The HTML5 Herald</title>
           <meta name="description" content="The HTML5 Herald">
           <meta name="author" content="SitePoint">
         <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-  
+
           <link rel="stylesheet" href="css/styles.css?v=1.0">
-  
+
         </head>
-  
+
         <body>
           <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
                 </div>
@@ -38,7 +43,6 @@ export async function sendEmail(req, res) {
         </body>
         </html>`,
       });
-      console.log(process.env.NEXT_PUBLIC_MAIL_API_KEY);
     } catch (error) {
       console.log(error);
       return res.status(error.statusCode).json({ error: error.message });
