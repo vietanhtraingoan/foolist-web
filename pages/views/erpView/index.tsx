473b67c723fd9a42cdfa872063d851785773223c
dangtuanbinh <tuanbinh.35@gmail.com>
2023-06-23 18:26:09 +0700

    fix erp

--- pages/views/erpView/index.tsx
+++ pages/views/erpView/index.tsx
@@ -15,6 +15,20 @@ const ERPView = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
 
   const { subImg, content } = ServicesMock[3];
 
+  const keyStr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63);
+
+  const rgbDataURL = (r: number, g: number, b: number) =>
+    `data:image/gif;base64,R0lGODlhAQABAPAA${
+      triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
   return (
     <div className={classNamePrefix}>
       <div className={`${classNamePrefix}__content`}>
@@ -27,6 +41,8 @@ const ERPView = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
             width={1150}
             height={850}
             loading="lazy"
+            placeholder="blur"
+            blurDataURL={rgbDataURL(237, 181, 6)}
           />
         </div>
 
